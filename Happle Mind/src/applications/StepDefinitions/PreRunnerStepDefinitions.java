eb0045d08328064d0702f356a1c0100619ffd318
Hifzur Rahman <hifzurrahman05081990@gmail.com>
2016-08-25 11:27:38 +0530

    PreRunner

--- Happle Mind/src/applications/StepDefinitions/PreRunnerStepDefinitions.java
+++ Happle Mind/src/applications/StepDefinitions/PreRunnerStepDefinitions.java
@@ -2,6 +2,7 @@ package applications.StepDefinitions;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -43,4 +44,55 @@ public class PreRunnerStepDefinitions extends BasePage{
 		ScenarioData.add(arrFeatureID);
 		ScenarioData.add(FeatureScenario.getName());		
 	}
+	
+	/*@Test
+	public void testCase1() {
+		System.out.println("in test case 1");
+	}*/
+
+	// test case 2
+//	@Test
+//	public void testCase2() {
+//		System.out.println("in test case 2");
+//	}
+
+	/*@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("in beforeMethod");
+	}
+//
+//	@AfterMethod
+//	public void afterMethod() {
+//		System.out.println("in afterMethod");
+//	}*/
+//
+//	@BeforeClass
+//	public void beforeClass() {
+//		System.out.println("in beforeClass");
+//	}
+//
+//	@AfterClass
+//	public void afterClass() {
+//		System.out.println("in afterClass");
+//	}
+//
+//	@BeforeTest
+//	public void beforeTest() {
+//		System.out.println("in beforeTest");
+//	}
+//
+//	@AfterTest
+//	public void afterTest() {
+//		System.out.println("in afterTest");
+//	}
+
+	/*@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("in beforeSuite");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("in afterSuite");
+	}*/
 }
