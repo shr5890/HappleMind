f996ae369b150bfb90c60515b473e067b83c34b1
Hifzur Rahman <hifzurrahman05081990@gmail.com>
2017-02-17 18:44:09 +0530

    Alignment

--- Happle Mind/src/applications/MethodsLibrary/ApplicationLibrary/MethodsLibrary.java
+++ Happle Mind/src/applications/MethodsLibrary/ApplicationLibrary/MethodsLibrary.java
@@ -1,11 +1,10 @@
-	package applications.MethodsLibrary.ApplicationLibrary;
+package applications.MethodsLibrary.ApplicationLibrary;
 
 public class MethodsLibrary {	
 	public static void method1(){
 		try{
 			Thread.sleep(2000);
 			Thread.sleep(2000);
-//			System.out.println("Step Def written");
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -15,30 +14,29 @@ public class MethodsLibrary {
 		try{
 			Thread.sleep(1000);
 			if(true){
-			System.out.println("Step Def2 written");				
-			System.out.println("Step Def1 written");
-			System.out.println("Step Def2A written");
+				System.out.println("Step Def2 written");				
+				System.out.println("Step Def1 written");
+				System.out.println("Step Def2A written");
 			}
 			if(!false){
-			System.out.println("Step Def3 written");
-			System.out.println("Step Def4A written");
-			System.out.println("Step Def4B written");
-			System.out.println("Step Def4C written");
-			System.out.println("Step Def4D written");
-			System.out.println("Step Def4E written");
-			System.out.println("Step Def4F written");
-			System.out.println("Step Def4G written");
-			System.out.println("Step Def5A written");
-			System.out.println("Step Def5B written");
-			System.out.println("Step Def5C written");
-            System.out.println("Step Def5D written");
-            System.out.println("Step Def5E written");
-            System.out.println("Step Def5F written");
-            System.out.println("Step Def5G written");
-			System.out.println("Step Def6 written");
-			System.out.println("Step Def7 written");
+				System.out.println("Step Def3 written");
+				System.out.println("Step Def4A written");
+				System.out.println("Step Def4B written");
+				System.out.println("Step Def4C written");
+				System.out.println("Step Def4D written");
+				System.out.println("Step Def4E written");
+				System.out.println("Step Def4F written");
+				System.out.println("Step Def4G written");
+				System.out.println("Step Def5A written");
+				System.out.println("Step Def5B written");
+				System.out.println("Step Def5C written");
+				System.out.println("Step Def5D written");
+				System.out.println("Step Def5E written");
+				System.out.println("Step Def5F written");
+				System.out.println("Step Def5G written");
+				System.out.println("Step Def6 written");
+				System.out.println("Step Def7 written");
 			}
-//			System.out.println("Step Def written");
 		}
 		catch(Exception e){
 			e.printStackTrace();
